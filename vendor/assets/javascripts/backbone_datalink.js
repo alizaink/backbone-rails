5f19ae99537dd05252a02214834381a458de9c8d
alizaink <alizaink@users.noreply.github.com>
2016-11-23 21:06:20 +0500

    Fix issue.

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -2,25 +2,26 @@
   return $.extend($.fn, {
     backboneLink: function(model) {
       return $(this).find(":input").each(function() {
-        var el, name;
-        el = $(this);
-        name = el.attr("name");
+        var that = this;
+        var el = $(that);
+        var name = el.attr("name");
         model.bind("change:" + name, function() {
           var start, end;
           
-          if (el.selectionStart && el.selectionEnd) {
-            start = el.selectionStart;
-            end = el.selectionEnd;
+          if (that.selectionStart && that.selectionEnd) {
+            start = that.selectionStart;
+            end = that.selectionEnd;
           }
           
           var retVal = el.val(model.get(name));
           
-          if (el.setSelectionRange)
-            el.setSelectionRange(start, end);
+          if (that.setSelectionRange)
+            that.setSelectionRange(start, end);
           
           return retVal;
         });
-        return $(this).bind("keyup", function() {
+        
+        return $(this).bind("change keyup", function() {
           var attrs;
           el = $(this);
           attrs = {};
