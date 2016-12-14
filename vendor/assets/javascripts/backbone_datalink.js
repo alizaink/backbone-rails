5bd2181649c5b091a2679bc7e6dd3f908dd13548
alizaink <alizaink@users.noreply.github.com>
2016-12-14 17:50:42 +0500

    Add nested model functionality

--- vendor/assets/javascripts/backbone_datalink.js
+++ vendor/assets/javascripts/backbone_datalink.js
@@ -12,12 +12,26 @@
         return $(this).bind("change input", function() {
           var attrs;
           var value;
+          var attr_name;
+          var modelNames;
+          var currentModel;
+          
           el = $(this);
           value = this.type == "checkbox" ? this.checked : el.val();
           attrs = {};
+          currentModel = model;
           
-          if (el.attr("name"))
-            attrs[el.attr("name")] = value;
+          if (el.attr("name")) {
+            attr_name = el.attr("name");
+            modelNames = attr_name.split('.')
+            attr_name = modelNames.pop()
+
+            modelNames.forEach(function(modelName) {
+              currentModel = currentModel.get(modelName);
+            });
+            
+            attrs[attr_name] = value;
+          }
           
           return model.set(attrs);
         });
